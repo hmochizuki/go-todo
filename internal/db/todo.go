010cb37c54b0250755edd3f893def4bab42ec7c7
hi mochizuki <hi.mochizuki@buysell-technologies.com>
2025-01-02 21:32:49 +0900

    feat: todo に status を追加

--- internal/db/todo.go
+++ internal/db/todo.go
@@ -5,8 +5,9 @@ import "time"
 type Todo struct {
 	ID        uint      `gorm:"primaryKey;comment:TODOのID"`
 	Name      string    `gorm:"comment:TODOの名称"`
-	UserID    string    `gorm:"not null;comment:ユーザーID(外部キー);index"`            // UserIDを外部キーとして設定
-	User      User      `gorm:"foreignKey:UserID;references:ID;comment:ユーザー情報"` // UserIDを外部キーとしてUserテーブルを参照
+	UserID    string    `gorm:"not null;comment:ユーザーID(外部キー);index"`
+	User      User      `gorm:"foreignKey:UserID;references:ID;comment:ユーザー情報"`
+	Status    string    `gorm:"type:enum(todo,doing,pending,archived,done);default:todo;comment:TODOのステータス"`
 	CreatedAt time.Time `gorm:"autoCreateTime;comment:作成日時"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime;comment:更新日時"`
 	Deleted   bool      `gorm:"default:false;comment:削除フラグ"`
